d88435c9763ca59ce2214f7297ab486a2e3af7dc
tiecia <ty.cia@outlook.com>
2025-04-19 22:30:04 -0700

    add course

--- flake.nix
+++ flake.nix
@@ -1,71 +1,3 @@
-# {
-#   description = "Flutter environment";
-#
-#   inputs = {
-#     flake-utils.url = "github:numtide/flake-utils";
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-#     android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
-#   };
-#
-#   outputs = { self, nixpkgs, flake-utils, android-nixpkgs }:
-#     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-#       let
-#         pkgs = import nixpkgs {
-#           inherit system;
-#           config.allowUnfree = true;
-#           android_sdk.accept_license = true;
-#         };
-#         androidSdk =  android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
-#           build-tools-35-0-0
-#           build-tools-34-0-0
-#           cmdline-tools-latest
-#           emulator
-#           platform-tools
-#           platforms-android-35
-#           platforms-android-34
-#           system-images-android-35-google-apis-x86-64
-#         ]);
-#         jdk = pkgs.jdk17;
-#         flutter = pkgs.flutter;
-#       in
-#       {
-#         devShell = pkgs.mkShell rec {
-#           ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-#           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-#           JAVA_HOME = jdk.home;
-#           FLUTTER_ROOT = flutter;
-#           DART_ROOT = "${flutter}/bin/cache/dart-sdk";
-#           GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/33.0.2/aapt2";
-#           QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X.
-#
-#           ANDROID_AVD_HOME = "/home/tiec/.config/.android/avd";
-#           # NB: due to the emulator's bundled qt version, it currently does not start with QT_QPA_PLATFORM="wayland".
-#           # Maybe one day this will be supported.
-#           buildInputs = [
-#             androidSdk
-#             flutter
-#             pkgs.qemu_kvm
-#             pkgs.gradle
-#             jdk
-#             pkgs.firebase-tools
-#           ];
-#           # emulator related: vulkan-loader and libGL shared libs are necessary for hardware decoding
-#           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.vulkan-loader pkgs.libGL]}";
-#           # Globally installed packages, which are installed through `dart pub global activate package_name`,
-#           # are located in the `$PUB_CACHE/bin` directory.
-#           shellHook = ''
-#             if [ -z "$PUB_CACHE" ]; then
-#               export PATH="$PATH:$HOME/.pub-cache/bin"
-#             else
-#               export PATH="$PATH:$PUB_CACHE/bin"
-#             fi
-#           '';
-#         };
-#       }
-#     );
-# }
-
-
 {
   description = "Flutter 3.13.x";
   inputs = {
@@ -92,6 +24,8 @@
           platforms-android-35
           platforms-android-34
           system-images-android-35-google-apis-x86-64
+          ndk-26-3-11579264
+          cmake-3-22-1
         ]);
         chrome-variant = "chromium";
         chrome-pkg = pkgs.${chrome-variant};
@@ -106,13 +40,16 @@
             ANDROID_HOME = "${android-sdk}/libexec/android-sdk";
             ANDROID_AVD_HOME = "/home/tiec/.config/.android/avd";
             CHROME_EXECUTABLE = "${chrome-script}";
-            GRADLE_USER_HOME = "/home/tiec/.gradle";
+            # GRADLE_USER_HOME = "/home/tiec/.gradle";
+            # Flutter SDK needs to be writable because of: https://github.com/NixOS/nixpkgs/issues/395096
+            FLUTTER_ROOT = "/home/tiec/.flutter-sdk/flutter";
             GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/35.0.0/aapt2";
             buildInputs = [
               pkgs.flutter
               android-sdk
               pkgs.jdk17
               chrome-pkg
+              pkgs.firebase-tools
             ];
           };
     });
---
+++ flutter_firebase_course/android/app/src/main/kotlin/com/flutter/course/flutter_firebase_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter.course.flutter_firebase_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
